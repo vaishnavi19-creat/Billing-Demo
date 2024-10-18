de88648afba2aeae4c01f15ab6878bc9a9068253
Vaishnavi Gabhane <129215788+vaishnavi19-creat@users.noreply.github.com>
2024-10-18 09:48:46 +0530

    Update Vendor.entities.ts

--- src/db/entities/Vendor.entities.ts
+++ src/db/entities/Vendor.entities.ts
@@ -71,28 +71,3 @@ export class Vendor extends CBaseEntities {
 
 
 
-// import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
-// import { CBaseEntities } from './CBase.entities';
-// import { UserEntities } from './User.entities';
-
-// @Entity('vendors')
-// export class Vendor extends CBaseEntities {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column()
-//   name: string;
-
-//   @Column()
-//   email: string;
-
-//   @Column()
-//   phoneNumber: string;
-
-//   @Column({ nullable: true })
-//   address: string;
-
-//   @ManyToOne(() => UserEntities, (user) => user.vendors)
-//   user: UserEntities; // Relation to the user entity who manages the vendor
-//     userId: number;
-// }
